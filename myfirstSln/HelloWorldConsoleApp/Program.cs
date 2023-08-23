8dcf945f2d0ecb7997d4df65b5d34fb1557df5ec
Juan Esteban Angulo Uribe <jangulorama@gmail.com>
2023-08-22 20:43:21 -0500

    Initial project Hello world

---
+++ myfirstSln/HelloWorldConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using HelloWorldConsoleApp;
+
+public static class MyFisrtConsoleApp{
+
+    public static void Main()
+    {
+        var person= GetPerson();
+        Console.WriteLine($"Hola {person.Name} tienes {person.Age} años");
+    }
+
+    public static PersonModel GetPerson()
+    {
+        var person= new PersonModel();
+        person.Name = "Jhon";
+        person.Age = 18;
+
+        return person;
+    }
+
+}
